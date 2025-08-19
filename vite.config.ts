3de7b68e7661740db042bb7713aff5bf1df9f115
Okelo123 <brianokelo3583@gmail.com>
2025-08-19 12:54:49 +0300

    vite-base-url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: Process.env.VITE_BASE_URL || '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
